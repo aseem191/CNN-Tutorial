179ed544d941e9f29eb9d294c7283e16ce9766ee
Aseem Sane <aseem191@gmail.com>
2018-07-25 18:25:05 -0700

    Add files via upload
    Added sections for with and without dropout.

--- cnntutorial.py
+++ cnntutorial.py
@@ -60,20 +60,103 @@ batch_size = 64
 epochs = 20
 num_classes = 10
 
+
+# no dropout
+
+# fashion_model = Sequential()
+# fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+# fashion_model.add(LeakyReLU(alpha=0.1))
+# fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+# fashion_model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+# fashion_model.add(LeakyReLU(alpha=0.1))
+# fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+# fashion_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+# fashion_model.add(LeakyReLU(alpha=0.1))                  
+# fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+# fashion_model.add(Flatten())
+# fashion_model.add(Dense(128, activation='linear'))
+# fashion_model.add(LeakyReLU(alpha=0.1))                  
+# fashion_model.add(Dense(num_classes, activation='softmax'))
+# fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+# fashion_model.summary()
+
+# fashion_train = fashion_model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
+
+# test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=0)
+# print('Test loss:', test_eval[0])
+# print('Test accuracy:', test_eval[1])
+
+
+
+# with dropout
+
 fashion_model = Sequential()
-fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',padding='same',input_shape=(28,28,1)))
 fashion_model.add(LeakyReLU(alpha=0.1))
 fashion_model.add(MaxPooling2D((2, 2),padding='same'))
+fashion_model.add(Dropout(0.25))
 fashion_model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
 fashion_model.add(LeakyReLU(alpha=0.1))
 fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Dropout(0.25))
 fashion_model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
 fashion_model.add(LeakyReLU(alpha=0.1))                  
 fashion_model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+fashion_model.add(Dropout(0.4))
 fashion_model.add(Flatten())
 fashion_model.add(Dense(128, activation='linear'))
-fashion_model.add(LeakyReLU(alpha=0.1))                  
+fashion_model.add(LeakyReLU(alpha=0.1))           
+fashion_model.add(Dropout(0.3))
 fashion_model.add(Dense(num_classes, activation='softmax'))
-fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
-
 fashion_model.summary()
+
+fashion_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+fashion_train_dropout = fashion_model.fit(train_X, train_label, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_X, valid_label))
+
+fashion_model.save("fashion_model_dropout.h5py")
+
+test_eval = fashion_model.evaluate(test_X, test_Y_one_hot, verbose=1)
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+accuracy = fashion_train_dropout.history['acc']
+val_accuracy = fashion_train_dropout.history['val_acc']
+loss = fashion_train_dropout.history['loss']
+val_loss = fashion_train_dropout.history['val_loss']
+epochs = range(len(accuracy))
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+predicted_classes = fashion_model.predict(test_X)
+
+predicted_classes = np.argmax(np.round(predicted_classes),axis=1)
+predicted_classes.shape, test_Y.shape
+
+correct = np.where(predicted_classes==test_Y)[0]
+print "Found %d correct labels" % len(correct)
+for i, correct in enumerate(correct[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(test_X[correct].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[correct], test_Y[correct]))
+    plt.tight_layout()
+
+incorrect = np.where(predicted_classes!=test_Y)[0]
+print "Found %d incorrect labels" % len(incorrect)
+for i, incorrect in enumerate(incorrect[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(test_X[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], test_Y[incorrect]))
+    plt.tight_layout()
+
+from sklearn.metrics import classification_report
+target_names = ["Class {}".format(i) for i in range(num_classes)]
+print(classification_report(test_Y, predicted_classes, target_names=target_names))
